d9925811970e3c66219e9f7f02f563992db18340
Ajay Kiran Sudharshanam <ajay.kiran.sudharshanam@concordusa.com>
2019-10-04 15:47:59 -0500

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="main-layout">
+
+      <header>
+        <h1>Header</h1>
+      </header>
+
+
+      <nav>
+        <h1>Nav</h1>
+      </nav>
+
+
+      <article>
+        <h1>London</h1>
+        <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+        <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+      </article>
+
+
+      <footer>
+        <p>Footer</p>
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
