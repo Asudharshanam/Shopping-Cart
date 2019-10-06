9388a150d1cd4017578f69a6d2f68f4cf3ea2cdc
Ajay Kiran Sudharshanam <ajay.kiran.sudharshanam@concordusa.com>
2019-10-06 15:47:50 -0500

    functionality for shopping cart

--- src/App.js
+++ src/App.js
@@ -2,34 +2,27 @@
 import React from 'react';
 
 import AppHeader from './components/AppHeader'
+import ItemListCards from './containers/ItemListCards';
+import Cart from './containers/Cart'
 
 import './App.css';
 
 function App() {
   return (
     <div className="main-layout">
-
       <header>
         <AppHeader />
+        <Cart />
       </header>
-
-
       <nav>
         <h1>Nav</h1>
       </nav>
-
-
       <article>
-        <h1>London</h1>
-        <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
-        <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+        <ItemListCards />
       </article>
-
-
       <footer>
         <p>Footer</p>
       </footer>
-
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
 import App from './App'
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const getReduxComposer = middleware => {
+    //EVIL LINE comment out before commiting
+    return (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose)(middleware)
+    // return compose(middleware)
+}
+
+const store = createStore(rootReducer, getReduxComposer(applyMiddleware(thunk)))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
 
 
 // If you want your app to work offline and load faster, you can change
